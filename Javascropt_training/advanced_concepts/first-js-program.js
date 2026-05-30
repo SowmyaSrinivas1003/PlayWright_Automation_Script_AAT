f69bc7530ef4f7495188b077e0e593e761af82c4
Sowmya Katta <kattasowmya1003@gmail.com>
2026-05-30 11:23:20 +0530

    Assignment1DataTypes

---
+++ Javascropt_training/advanced_concepts/first-js-program.js
@@ -0,0 +1,4 @@
+console.log("Hello world");
+console.info("This is info message.");
+console.warn("This is a warning message.");
+console.error("Thsi is a error message.");
---
+++ Javascropt_training/advanced_concepts/typescript-training/Assignment-1.ts
@@ -0,0 +1,100 @@
+//Assignment 1--Data Types
+
+//1. Temperature of a city in degrees Celsius: 25.5
+
+let temperature :number =25.5;
+console.log(temperature);
+//2. Whether a customer has placed an order: true or false
+
+let orderPlaced :boolean=true;
+console.log(orderPlaced);
+
+//3. Person's phone number: "123-456-7890"
+
+let personPhoneNumber: string ="123-456-7890";
+console.log(`Person's phone number: ${personPhoneNumber} `);
+
+//4. Amount of money in a customer's bank account: 1000.50
+
+let accountMoney :number =1000.50;
+console.log("Amount of money in a customer's bank account: "+accountMoney );
+
+//5. Person's email address: "john.doe@example.com"
+
+let emailAddress: String ="kattasowmya1003@gmail.com";
+console.log("Person's email address: ",emailAddress);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+let latitude :number=37.7749;
+let longitude:number =-122.4194;
+
+console.log(`Coordinates of a location (latitude, longitude) is : ( ${latitude}, ${longitude})`);
+
+//7. Person's marital status: true or false
+
+let maritalStatus :boolean =false;
+console.log("Person's marital status: true or false : ",maritalStatus);
+
+//8. Person's occupation: "Software Engineer"
+
+let personOccupation : string ="software Engineer";
+console.log("Person's occupation: "+personOccupation);
+
+//9. Person's favourite colour: "Blue"
+
+let favouriteColour : string="Yellow";
+console.log("Person's favourite colour: "+favouriteColour);
+
+//10.Current year: 2023
+
+let currentYear :number=2026;
+console.log("Current year: ",currentYear);
+
+//11.Number of followers on a social media platform: 1,000,000
+let countOfFollowers :number =1000000;
+console.log(`Number of followers on a social media platform: ${countOfFollowers}`);
+
+//12.Rating of a movie: 7.5
+
+let rating :number =7.5;
+console.log('Rating of a movie: ',rating);
+
+//13.Person's blood type: 'O'
+let bloodGroup :string='O';
+console.log(`Person's blood type: ${bloodGroup}`);
+
+//14.Title of a book: "To Kill a Mockingbird"
+
+let titleOfBook:String ="To Kill a Mockingbird";
+console.log('Title of a book: ',titleOfBook);
+
+//15.Number of employees in a company: 500
+
+let countOfEmp:number =500;
+console.log("Number of employees in a company:",countOfEmp);
+
+//16.Time of an event: 2:30 PM
+
+let eventTime :string="2:30 PM";
+console.log(`Time of an event: ${eventTime}`);
+
+//17.Name of a country: "United States"
+
+let countryName : String ="United States";
+console.log("Name of a country: "+countryName);
+
+//18.Person's eye color: "Brown"
+
+let eyeColour:String="Brown";
+console.log("Person's eye color: "+eyeColour);
+
+//19.Person's birthplace: "New York City"
+
+let birthPlace :String ="New York City";
+console.log("Person's birthplace: ",birthPlace);
+
+//20. Distance between two cities: 200.5
+
+let distanceOfTwoCities : number =200.5;
+console.log("Distance between two cities: "+distanceOfTwoCities);
